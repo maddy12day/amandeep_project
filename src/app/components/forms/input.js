a8dc74aa20f6683644b8bb44fcf81e879e549997
amandeepsingh1929 <as0632568@gmail.com>
2022-06-29 16:30:53 +0530

    Dynamic input field done

--- src/app/components/forms/input.js
+++ src/app/components/forms/input.js
@@ -1,13 +1,33 @@
-const Input =({type,placeholder,name})=>{
+import PropTypes from "prop-types";
+import { Image } from "react-bootstrap";
+const Input =({type,placeholder,name,inputClass,error,imgSrc})=>{
   return(
     <>
-      <div className="container">
+      <div className="div-container">
       <div className="input-container">
-      <input type={type} placeholder={placeholder} name={name}/>
+      {imgSrc !== null ? <Image src={imgSrc}/> : ""}
+      <input type={type} placeholder={placeholder} name={name} className={ error === null ? inputClass : 'error-input ' + inputClass } 
+      />
      </div>
       </div>
     </>
-  )
-}
+  );
+};
+
+Input.defaultProps = {
+  type: "text",
+  placeholder:null,
+  inputClass:"input",
+  name:null,
+  error:null,
+  imgSrc:null,
+};
+
+Input.propTypes = {
+  type: PropTypes.string,
+  placeholder: PropTypes.string,
+  name: PropTypes.string,
+  inputClass:PropTypes.string
+};
 
 export default Input;
--- src/app/views/Onboarding/onboarding.jsx
+++ src/app/views/Onboarding/onboarding.jsx
@@ -1,9 +1,14 @@
 import Input from "../../components/forms/input";
+import logo from '../../assets/images/user.png'
+import Select from "../../components/forms/select";
 
 const Onboarding = () => {
     return (
         <>
-        <Input type={"text"} placeholder={"Enter invite code"} name={"usercode"}/>
+        {/* <Input type={"text"} placeholder={"Enter invite code"} name={"usercode"}
+           inputClass={"start-input"} imgSrc={logo}
+        /> */}
+        <Select selectClass={"start-select"} dataArr={[{label:'option 1',value:'option1'}]}/>
         </>
     )
 }
