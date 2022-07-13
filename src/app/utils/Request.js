f3ec318c87984e0a865ee2efb03a1df0dfb41f3a
Akashdeep  Singh <akashdeep@hiehq.com>
2022-07-13 17:30:15 +0530

    Fix files

--- src/app/utils/Request.js
+++ src/app/utils/Request.js
@@ -1,10 +1,10 @@
-import axios from 'axios';
+// import axios from 'axios';
 
 const url = process.env.REACT_APP_API_BASE_URL 
 
-export default axios.create({
-    baseURL: url
-})
+// export default axios.create({
+//     baseURL: url
+// })
 
 export const handleApi = async(promise) => {
     return promise.then((result) => result.data )
