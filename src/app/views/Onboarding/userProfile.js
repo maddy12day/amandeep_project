9e59591731f485e0fe66c28d896f9b1e477a95a0
amandeepsingh1929 <as0632568@gmail.com>
2022-08-08 15:59:37 +0530

    In progress user-rofile page .

--- src/app/views/Onboarding/userProfile.js
+++ src/app/views/Onboarding/userProfile.js
@@ -5,21 +5,42 @@ import userTwitter from '../../assets/images/twitter-user.png'
 import userLinked from "../../assets/images/linked-user.png"
 import userSocial from "../../assets/images/user-social.png"
 import userTwitch from "../../assets/images/twitch-user.png"
+import userIcon from "../../assets/images/icon.png"
+import userVideo from "../../assets/images/user-video.png"
+import userSnap from "../../assets/images/user-snap.png"
+import userSea from '../../assets/images/user-sea.png'
+import userCash from "../../assets/images/user-cash.png"
+import userTumblur from "../../assets/images/user-tumblr.png"
+import userSound from "../../assets/images/user-sound.png"
+import { useState } from 'react'
 const UserProfile=()=>{
+
+const[flag,setFlag]=useState(true);
+
+  const handlechange=()=>{
+     setFlag(!flag);
+     
+
+  }
+
+  // const handleChange1=()=>{
+  //   setFlag(true)
+  // }
   return(
     <>
-      <div className="user-container">
+      <div className={`user-container
+     ${flag === false ? "user-container1" : ""} `}>
         <div className="user-background">
          <img src={userBg} alt="Background"
           className='background-image'
          />
         </div>
-        <div className='user-image'>
+        {/* <div className='user-image'>
      <img src={userImg}  alt="user"
       className='user-img'
      />
-        </div>
-        <div className='user-social'>
+        </div> */}
+        <div className={flag === false? 'user-social1' :'user-social'}>
         <img src={userInsta} alt="social"
           className='socials-user'
         />
@@ -43,11 +64,56 @@ const UserProfile=()=>{
          </p>
         </div>
         <div className='user-links'>
-       <p>See All</p>
+       {flag === false ? <p onClick={handlechange}>Hide</p>:<p onClick={handlechange}>See All</p>}
+       {/* {flag === true ? <p>See All</p>:""} */}
         </div>
-        <div className='user-'>
-
-        </div>
+        { flag === true ?
+        <div>
+        <div className='user-media'>
+         <img src={userTwitch} alt="media" className='user-media-img'/>
+         <p className='user-media-para'>Twitch</p>
+        </div> 
+        <div className='user-media '>
+         <img src={userIcon} alt="media" className='user-media-img'/>
+         <p className='user-media-para'>Follow me on FanClub</p>
+        </div> 
+        <div className='user-media '>
+         <img src={userVideo} alt="media" className='user-media-img'/>
+         <p className='user-media-para'>1:1 video chat</p>
+        </div></div>:<div>
+        <div className='user-media'>
+         <img src={userTwitch} alt="media" className='user-media-img'/>
+         <p className='user-media-para user-para'>Twitch</p>
+        </div> 
+        <div className='user-media'>
+         <img src={userSnap} alt="media" className='user-media-img'/>
+         <p className='user-media-para'>Snapchat</p>
+        </div> 
+        <div className='user-media'>
+         <img src={userSea} alt="media" className='user-media-img'/>
+         <p className='user-media-para'>Open Sea</p>
+        </div> 
+        <div className='user-media'>
+         <img src={userCash} alt="media" className='user-media-img'/>
+         <p className='user-media-para'>Cash App</p>
+        </div> 
+        <div className='user-media'>
+         <img src={userTumblur} alt="media" className='user-media-img'/>
+         <p className='user-media-para user-para'>Tumblr</p>
+        </div> 
+        <div className='user-media'>
+         <img src={userSound} alt="media" className='user-media-img user-image'/>
+         <p className='user-media-para'>Sound Cloud</p>
+        </div> 
+        <div className='user-media'>
+         <img src={userIcon} alt="media" className='user-media-img'/>
+         <p className='user-media-para'>Follow me on FanClub</p>
+        </div> 
+        <div className='user-media'>
+         <img src={userVideo} alt="media" className='user-media-img'/>
+         <p className='user-media-para'>1:1 video chat</p>
+        </div> 
+        </div>} 
       </div>
     </>
   )
